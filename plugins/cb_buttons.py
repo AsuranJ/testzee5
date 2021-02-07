2fd34dc7f94277295f61fc9e8cb5b58258ba1be7
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-08 01:43:00 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -1,7 +1,7 @@
 import pyrogram
 
 from plugins.zee5_dl import zee5_execute
-from plugins.zee5_dlm import zee5_execute
+from plugins.zee5_dlm import zee5m_execute
 
 
 @pyrogram.Client.on_callback_query()
